ff2da717c84bd027528d34103cd8a01f36159611
StarKos871 <59029478+StarKos871@users.noreply.github.com>
2020-01-13 00:08:58 +0300

    feat: add modal dialog

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
+
   const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-togle=modal]');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
